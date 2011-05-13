9bd33f139555b82280d977562ddbac5b77e52bbc
tassielocost <tassielocost@gmail.com>
2011-05-13 09:32:36 +0000

    Further Developments

---
+++ CCMManager/ViewModels/ComputerDetailsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CCMManager.ViewModels
+{
+    using Caliburn.Micro;
+    using Framework;
+    using Models;
+    using System.ComponentModel.Composition;
+
+    [Export(typeof(ComputerDetailsViewModel))]
+    [Export(typeof(IChildScreen<ActionsViewModel>))]
+    public class ComputerDetailsViewModel : Screen, IChildScreen<ActionsViewModel>
+    {
+        [ImportingConstructor]
+        public ComputerDetailsViewModel()
+        {
+
+        }
+
+        public int? Order
+        {
+            get { return 1; }
+        }
+
+        public string ScreenId
+        {
+            get { return GetType().Name; }
+        }
+    }
+}
